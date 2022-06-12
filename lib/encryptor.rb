58b8bf21a024646cb91bc507056efeb3fef83b29
Tyler Caudill <teecaud@hotmail.com>
2022-06-12 11:56:26 -0500

    add and test character set

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,10 +1,11 @@
 class Encryptor
 
-  attr_reader :message, :key, :date
+  attr_reader :message, :key, :date, :character_set
   def initialize(message, key, date)
     @message = message
     @key = key
     @date = date
+    @character_set = ("a".."z").to_a <<  " "
 
   end
 end
--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -1,4 +1,5 @@
 require './lib/encryptor'
+require './lib/enigma'
 
 RSpec.describe Encryptor do
   before :each do
@@ -10,5 +11,9 @@ RSpec.describe Encryptor do
     expect(@encrypt.message).to eq("hello world")
     expect(@encrypt.key).to eq("02715")
     expect(@encrypt.date).to eq("040895")
+  end
+
+  it "has a character set array by default" do
+    expect(@encrypt.character_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
   end
 end
