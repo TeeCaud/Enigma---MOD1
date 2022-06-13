891493743f80adea60f518bf18be1a11ef401360
Tyler Caudill <teecaud@hotmail.com>
2022-06-12 19:36:00 -0500

    feat/test attributes and character set

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -1,6 +1,11 @@
 class Decryptor
 
-  def initialize
+  attr_reader :message, :key, :date, :character_set
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+    @character_set = ("a".."z").to_a <<  " "
 
   end
 end
--- spec/decryptor_spec.rb
+++ spec/decryptor_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Decryptor do
 
   it "exists with attributes" do
     expect(@decrypt).to be_instance_of(Decryptor)
+    expect(@decrypt.message).to eq("keder ohulw")
+    expect(@decrypt.key).to eq("02715")
+    expect(@decrypt.date).to eq("040895")
+  end
+
+  it "has a character set array by default" do
+    expect(@decrypt.character_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
   end
 end
